78e8194a7069b690d76116af5660d40164f7d604
JoseABCruz <joseantoniobalbuenacruz@gmail.com>
2023-11-22 13:27:23 -0600

    Revert "cambios"
    This reverts commit 66d2206f5d2f253f9386bd346d6ba4aba9d07fbd.

--- App.tsx
+++ App.tsx
@@ -1,39 +1,118 @@
-import 'react-native-gesture-handler';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-// import { StackNavigation } from './src/navigation/StackNavigation';
-// import { MenuLateralBasico } from './src/navigation/MenuLateralBasico';
-import { MenuLateral } from './src/navigation/MenuLateral';
-import { Tabs } from './src/navigation/Tabs';
-import { AuthProvider } from './src/context/AuthContext';
-import { FeedScreen } from './src/screens/FeedScreen';
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
 
-const App = () => {
+function Section({children, title}: SectionProps): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <NavigationContainer>
-      <AppState>
-        {/* <Text>Hola navegación</Text> */}
-        {/* <StackNavigation /> */}
-        {/* <MenuLateralBasico /> */}
-        {/* Principal */}
-        <MenuLateral /> 
-        {/* <FeedScreen /> */}
-        {/* <Tabs /> */}
-      </AppState>
-    </NavigationContainer>
-    
-  )
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
 }
 
-const AppState = ({children}: any) => {
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
   return (
-    <AuthProvider>
-      {children}
-    </AuthProvider>
-  )
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
 
 export default App;
